412646ce76d27243f3eeebce802f2c9f7c8635cf
aynursune <aynur.suene@tum.de>
2020-07-01 01:16:29 +0200

    Cleaning

--- Util.py
+++ Util.py
@@ -36,9 +36,10 @@ def mse(y_truth, y_pred):
 
 def distance_matrix(x):
     """
+    Creates distance matrix from given vector.
 
-    :param x:
-    :return:
+    :param x: Vector [N,].
+    :return: Matrix [N,N]
     """
     n = x.shape[0]
     dist_matrix = np.zeros((n, n))
@@ -58,53 +59,6 @@ def linear_approximation(x, fx):
     """
     A, res, rnk, s = lstsq(x, fx)
     return A
-
-def plot_mse_vs_epsilon_and_l_task5(fx_approximated_linear, fx_linear, x_linear_new):
-    linear_approximation_error = mse(fx_linear, fx_approximated_linear)  # mse=1.0604702468531419e-10
-    # Calculate MSE for each epsilon value
-    L = 20
-    es = np.linspace(0.1, 1, 1000)
-    mess = np.zeros(es.shape)
-    for index, epsilon in enumerate(es):
-        C, phis = nonlinear_approximation(x_linear_new, fx_linear[:, np.newaxis], epsilon, L, [])
-        fx_approximated_nonlinear = np.matmul(phis, C)
-
-        nonlinear_approximation_error = mse(fx_linear, fx_approximated_nonlinear.reshape(-1))
-        error_difference = nonlinear_approximation_error - linear_approximation_error
-        print("epsilon: {}, linear error: {}, nonlinear error: {} difference: {}"
-              .format(epsilon, linear_approximation_error, nonlinear_approximation_error, error_difference))
-        mess[index] = nonlinear_approximation_error
-    fig, ax = plt.subplots(1, 1)
-    ax.scatter(es, mess, color='indianred', s=5, label='Nonlinear MSE')
-    ax.plot(es, [linear_approximation_error] * len(es), linewidth=0.5, color='dodgerblue', label='Linear MSE')
-    ax.set_title('L = {}'.format(L))
-    ax.set_xlabel('$\epsilon$')
-    ax.set_ylabel('MSE')
-    plt.legend()
-    plt.show()
-    # Calculate MSE for each L value
-    Ls = np.arange(1, 1000, 2)
-    mess = np.zeros(Ls.shape)
-    #epsilon = np.sqrt(np.max(D)) * 0.05
-    epsilon = 0.8
-    for index, L in enumerate(Ls):
-        C, phis = nonlinear_approximation(x_linear_new, fx_linear[:, np.newaxis], epsilon, L, [])
-        fx_approximated_nonlinear = np.matmul(phis, C)
-
-        nonlinear_approximation_error = mse(fx_linear, fx_approximated_nonlinear.reshape(-1))
-        error_difference = nonlinear_approximation_error - linear_approximation_error
-        print("L: {}, linear error: {}, nonlinear error: {} difference: {}"
-              .format(L, linear_approximation_error, nonlinear_approximation_error, error_difference))
-        mess[index] = nonlinear_approximation_error
-    fig, ax = plt.subplots(1, 1)
-    ax.scatter(Ls, mess, color='indianred', s=5, label='Nonlinear MSE')
-    ax.plot(Ls, [linear_approximation_error] * len(Ls), linewidth=0.5, color='dodgerblue', label='Linear MSE')
-    ax.set_title('$\epsilon$ = {}'.format(epsilon))
-    ax.set_xlabel('L')
-    ax.set_ylabel('MSE')
-    plt.legend()
-    plt.show()
-
 
 
 def nonlinear_approximation(x, fx, epsilon, L, xl):
